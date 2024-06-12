447bfb1fa6ac4c32502a179b999eafeeb3b773d7
tomass <dominanthat@gmail.com>
2024-06-12 14:21:48 +0900

    feat(DTO): 토큰을 받기 위한 DTO 파일들 생성
    - 토큰을 받기 위해서 리퀘스트를 만들어주기 위한 GoogleRequest.java
    - 토큰을 받아올 GoogleResponse.java
    - 토큰을 바탕으로 개인정보를 받기 위한 GoogleInfResponse.java

---
+++ src/main/java/com/example/todo/googleDTO/GoogleInfResponse.java
@@ -0,0 +1,58 @@
+package com.example.todo.googleDTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class GoogleInfResponse {
+    @JsonProperty("iss")
+    private String iss;
+
+    @JsonProperty("azp")
+    private String azp;
+
+    @JsonProperty("aud")
+    private String aud;
+
+    @JsonProperty("sub")
+    private String sub;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("email_verified")
+    private String emailVerified;
+
+    @JsonProperty("at_hash")
+    private String atHash;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("picture")
+    private String picture;
+
+    @JsonProperty("given_name")
+    private String givenName;
+
+    @JsonProperty("family_name")
+    private String familyName;
+
+    @JsonProperty("locale")
+    private String locale;
+
+    @JsonProperty("iat")
+    private String iat;
+
+    @JsonProperty("exp")
+    private String exp;
+
+    @JsonProperty("alg")
+    private String alg;
+
+    @JsonProperty("kid")
+    private String kid;
+
+    @JsonProperty("typ")
+    private String typ;
+}
